e8a3af0fd1d875318ea83682a43fc79d646ca16d
Mike Gajda <mgajda@bu.edu>
2019-05-14 23:31:47 -0400

    Download a file and upload to S3

--- services/s3.ts
+++ services/s3.ts
@@ -1,23 +1,22 @@
-import * as fs from "fs";
 import * as path from "path";
 
 const aws = require("aws-sdk");
 const uuid = require('uuid');
 require('dotenv').config();
-
+const axios = require('axios')
 
 const uploadPhotoToS3 = async (url: string) => {
 
     // Create unique bucket name
-    let bucketName: string = uuid.v4();
+    let bucketName: string = "endeavor-mgajda-test-bucket";
     // Create name for uploaded object key
     let keyName: string = uuid.v4();
 
     let s3: any = new aws.S3({apiVersion: '2006-03-01'});
 
-    let pathToImage: string = "/Users/mikegajda/LargeFiles/link-blog-backend/IMG_8612.JPG";
-
-    let image: Buffer = fs.readFileSync(pathToImage);
+    // let pathToImage: string = "";
+    //
+    // let image: Buffer = fs.readFileSync(pathToImage);
 
     try {
         // Create a promise on S3 service object
@@ -26,36 +25,32 @@ const uploadPhotoToS3 = async (url: string) => {
         throw Error(e)
     }
 
+    let response;
     try {
-        let objectParams = {
-                ACL: "public-read",
-                Bucket: bucketName,
-                ContentType: "binary",
-                Key: path.basename(pathToImage),
-                Body: image
-            }
-        ;
-        await s3.putObject(objectParams).promise();
+        response = await axios({
+            method: "GET",
+            url: url,
+            responseType: 'arraybuffer'
+        });
     } catch (e) {
         throw Error(e)
     }
 
 
-    // Handle promise fulfilled/rejected states
-    // bucketPromise.then(
-    //     function (data) {
-    //         // Create params for putObject call
-    //         var objectParams = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-    //         // Create object upload promise
-    //         var uploadPromise = new AWS.S3({apiVersion: '2006-03-01'}).putObject(objectParams).promise();
-    //         uploadPromise.then(
-    //             function (data) {
-    //                 console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-    //             });
-    //     }).catch(
-    //     function (err) {
-    //         console.error(err, err.stack);
-    //     });
+    try {
+        let objectParams = {
+                ACL: "public-read",
+                Bucket: bucketName,
+                ContentType: response.contentType,
+                Key: path.basename(url),
+                Body: response.data
+            }
+        ;
+        let s3response = await s3.putObject(objectParams).promise();
+        console.log(s3response)
+    } catch (e) {
+        throw Error(e)
+    }
 
 };
 
--- test/s3.js
+++ test/s3.js
@@ -2,6 +2,6 @@ const {uploadPhotoToS3} = require("../dist/s3");
 const assert = require('assert');
 
 describe("s3", () => {
-    uploadPhotoToS3("test")
+    uploadPhotoToS3("https://cdn.vox-cdn.com/thumbor/78eLaVQdVViXWGgISQcxhJEFm4Q=/0x0:2040x1360/1820x1213/filters:focal(857x517:1183x843):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/63835958/jbareham_170417_1617_0001.0.jpg")
 });
 
